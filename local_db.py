0ffcd03f67e31701682b95d5e1c2e7d3d98f8c56
G-Shinohara <guishinohara@gmail.com>
2026-05-13 21:25:42 -0400

    [NEW] mTLS

--- local_db.py
+++ local_db.py
@@ -1,13 +1,22 @@
 import sqlite3
-import time
 import paho.mqtt.client as mqtt
 from datetime import datetime
+from pathlib import Path
 
 # Configurações
 MQTT_BROKER = "localhost"
-MQTT_PORT = 1883
-MQTT_TOPIC = "seu/topico/aqui"  # Altere para o tópico desejado
+MQTT_PORT = 8883  # Porta segura
+MQTT_TOPIC = "esp32/rain_sensor"  # Altere para o tópico desejado
 DB_FILE = "dados_mqtt.db"
+
+# Caminhos dos certificados gerados pelo script config_mosquitto.py (~ = diretório home do usuário)
+_CERT_DIR = Path.home() / "mqtt_secure/clients/local_db"
+CLIENT_CERT = str(_CERT_DIR / "local_db.crt")
+CLIENT_KEY = str(_CERT_DIR / "local_db.key")
+CA_CERT = str(_CERT_DIR / "ca.crt")
+
+MQTT_USERNAME = "local_db"
+MQTT_PASSWORD = "1234"
 
 # Cria o banco e tabela se não existir
 conn = sqlite3.connect(DB_FILE)
@@ -35,6 +44,12 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+
+# Configura autenticação
+client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+
+# Configura TLS/mTLS
+client.tls_set(ca_certs=CA_CERT, certfile=CLIENT_CERT, keyfile=CLIENT_KEY)
 
 client.connect(MQTT_BROKER, MQTT_PORT, 60)
 print(f"Escutando tópico '{MQTT_TOPIC}' no broker {MQTT_BROKER}:{MQTT_PORT}...")
